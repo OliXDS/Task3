e401c08f08b80bd68ef66e7a130090c08b704d90
OliXDS <oliversygulski@gmail.com>
2025-01-18 20:58:18 +0000

    palindromeSimpleCheck

--- task3.py
+++ task3.py
@@ -1,1 +1,9 @@
-print("hello")
+def check_palindrome(word):
+    inverse = word[::-1]
+    if inverse == word:
+        return True
+    else: 
+        return False
+
+print(check_palindrome("kayak"))
+# kayak --> inverse and check from original
--- test_palindrome.py
+++ test_palindrome.py
@@ -1,1 +1,18 @@
-print("hello")
+from task3 import check_palindrome
+
+
+def test_is_palindrome():
+    assert check_palindrome("boat") == False
+    assert check_palindrome("kayak") == True
+    assert check_palindrome("Kayak") == True
+    assert check_palindrome("ka yak") == True
+
+
+
+# NOT A PALINDROME
+# boat --> toab 
+
+# IS A PALINDROME
+# kayak --> kayak
+# Kayak --> kayaK
+# ka yak --> kay ak
