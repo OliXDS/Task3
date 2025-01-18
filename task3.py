0842fb37b482a534a4ebd1395a472a72db35a0a4
OliXDS <oliversygulski@gmail.com>
2025-01-18 21:03:54 +0000

    caseSens

--- task3.py
+++ task3.py
@@ -1,9 +1,10 @@
 def check_palindrome(word):
+    word = word.lower()
     inverse = word[::-1]
     if inverse == word:
         return True
     else: 
         return False
 
-print(check_palindrome("kayak"))
+print(check_palindrome("Kayak"))
 # kayak --> inverse and check from original
