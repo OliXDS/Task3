97a26dddb44a79bf2e3a61a93596d15408bc8696
OliXDS <oliversygulski@gmail.com>
2025-01-18 21:07:19 +0000

    comments

--- task3.py
+++ task3.py
@@ -1,10 +1,20 @@
+# check if input is a palindrome 
 def check_palindrome(word):
+    # converts all characters to lowercase
     word = word.lower()
+
+
+    # stores the inverse of word
     inverse = word[::-1]
+    # checks is inverse is equal to the word
     if inverse == word:
         return True
+    # if word is not equal to the inverse then return False
     else: 
         return False
 
-print(check_palindrome("Kayak"))
-# kayak --> inverse and check from original
+
+# calling the function and passing a parameter
+print(check_palindrome("Ka yak"))
+
+
